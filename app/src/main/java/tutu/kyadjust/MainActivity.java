8caf5c3356a61164bc66a9b0dbca90eb57079b21
tuzhenyu <616871820@qq.com>
2017-12-11 00:42:24 +0800

    首次提交

---
+++ app/src/main/java/tutu/kyadjust/MainActivity.java
@@ -0,0 +1,26 @@
+package tutu.kyadjust;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import tutu.kayboardadjustlayout.R;
+
+/**
+ * Created by tutu on 2017/12/10.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conversation_fragment);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+}
